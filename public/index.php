0edef73a17d8e32f365ae718856c0b4e31010059
trangthanhphuc <trangthanhphuc91@gmail.com>
2023-04-04 18:50:32 +0700

    Update dang nhap

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use core\Application;
-
+use app\model\Movie;
+use core\View;
 // Initialize application
 $app = Application::init();
 
@@ -11,18 +12,25 @@ $app->router->get('/hi', "example.html");
 $app->router->get('/test', ["controller"=>"HomeController", "action"=>"homeAction"]);
 $app->router->get('/', ["controller"=>"HomeController", "action"=>"homePageRender"]);
 $app->router->get('/name/{name:\w+}', ["controller"=>"HomeController", "action"=>"otherAction"]);
-$app->router->get('/page/{id:\d+}', function($id){
+$app->router->get('/page/{id:\d+}', function($id){ 
     echo "Bạn đang truy cập trang $id";
 });
+
+$app->router->get('/phuc', function(){ 
+    View::render('dangnhap.php', ["username"=> "Phuc"]);
+});
+
+$app->router->get('/signin', ['controller' => 'SigninController', 'action' => 'getSignPage']);
 
 $app->router->get('/{error:\S+}', function($error){
     echo "Bạn đang truy cập trang $error không tồn tại"."<br>";
     echo "<a href='/'>Click vào đây để trở về</a>";
 });
 
+$app->router->post('/signin', ["controller" => "SigninController", "action" => "validateLogin"]);
 
 // Running and resolver
 $app->run();
 
-//$movie = Movie::find("movie100");
-//var_dump($movie);
+// $movie = Movie::find("movie100");
+// var_dump($movie);
---
+++ src/controller/SignInController.php
@@ -0,0 +1,56 @@
+<?php
+namespace app\controller;
+use core\Controller;
+use core\View;
+
+class SignInController extends Controller{
+
+    public function getSignPage(){
+        $navbar = GlobalController::getNavbar();
+
+        $listCinema =[
+            [
+                "title" => "Quá đã",
+                "year" => 2023,
+                "category" => "giật giật, đáng ghét",
+                "url" => "/assets/slider1.jpg"
+
+            ],
+            [
+                "title" => "Quá phê",
+                "year" => 2023,
+                "category" => "giật giật, đáng ghét",
+                "url" => "/assets/slider2.jpg"
+            ],
+            [
+                "title" => "Quá sướng",
+                "year" => 2023,
+                "category" => "giật giật, đáng ghét",
+                "url" => "/assets/slider3.jpg"
+            ],
+            
+        ];
+
+        View::renderTemplate('signIn/signIn_page.html',[
+            "navbar" => $navbar,
+            "listCinema" => $listCinema
+        ]);
+      
+    }
+
+    public function validateLogin(){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // $user = User::where("usernmae == $username AND password == $password");
+        // if ($user == null) echo "Sai mat khau hoac ten dang nhap"
+        // else "Thanh cong";
+
+        $defaultUsername = "phuc";
+        $defaultPassword = "123";
+
+        if ($defaultUsername == $username && $defaultPassword == $password)
+            echo "Thành công";
+        else echo "Thất bại";
+    }
+}
