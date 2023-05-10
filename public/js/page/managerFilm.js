1da038ab5111b26a2a995df5868357c2fb6c3e84
trangthanhphuc <trangthanhphuc91@gmail.com>
2023-05-10 14:05:51 +0700

    update giao diên  controller thêm movie, thêm file trong movie
    thêm file bằng cách: Tên quăng lên database, file nằm trong thư mục asset
    Todo-list: Chưa bắt lỗi về về file khi push lên

---
+++ public/js/page/managerFilm.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+    $(".default-option").click(function(){
+        $(".dropdown ul").toggleClass("active");
+    });
+
+    $(".dropdown ul li").click(function(){
+        var text = $(this).text();
+        $(".default-option").text(text);
+        $(".dropdown ul").removeClass("active");
+    });
+
+    
+    $('.deleteFilm').click(function() {
+        // Sử dụng toggle để hiển thị/ẩn thị modal-delete
+        $('.modal-delete').toggle();
+    });
+
+    // Lắng nghe sự kiện khi click vào nút đóng (x) hoặc nút Đóng
+    $('.close, .btn-secondary').click(function() {
+        // Sử dụng toggle để ẩn thị modal-delete
+        $('.modal-delete').toggle();
+    });
+  
+    //modal addFilm
+    $(".add-film").click(function() {
+      $("#Add").toggle();
+    });
+
+    $('.edit-film').click(function() {
+      $("#Edit").toggle();
+      console.log("testing");
+    });
+
+    // Khi click bên ngoài modal, đóng modal
+    $(window).on('click', function(event) {
+        if ($(event.target).is('.modal')) {
+          $(".modal").hide();
+        };
+
+        if ( $(event.target).is('.modalAddFilm')) {
+          $(".containerFilmAdd").hide();
+        }
+    });
+
+    
+    $("#btnAddFilm").click((e) => {
+        e.preventDefault();
+        var formData = new FormData();
+
+        let nameMovie = $("#name-movie-add").val();
+        let desMovie = $("#des-movie").val();
+        let posterLink = $("#poster-link")[0].files[0];
+        let landscapeLink = $("#landscape-poster")[0].files[0];
+        let trailerLink = $("#trailer-link").val();
+        let directors = $("#directors").val();
+        let actors = $("#actors").val();
+        let duringTime = $("#during-time").val();
+        let datePicker = $("#datepicker").val();
+        let language = $("#language").val();
+        let customSwitches = $("#customSwitches").is(':checked') ? '1' : '0';
+        
+        formData.append('nameMovie', nameMovie);
+        formData.append('desMovie', desMovie);
+        formData.append('posterLink', posterLink);
+        formData.append('landscapeLink', landscapeLink);
+        formData.append('trailerLink', trailerLink);
+        formData.append('directors', directors);
+        formData.append('actors', actors);
+        formData.append('duringTime', duringTime);
+        formData.append('datePicker', datePicker);
+        formData.append('language', language);
+        formData.append('customSwitches', customSwitches);
+
+
+        //array checkbox have value="checked"
+        var checkedInputs = $(".checkboxCategoryAdd");
+        var checkedIds = [];
+        checkedInputs.each(function() {
+          if ($(this).prop('checked')) {
+            checkedIds.push($(this).attr("id"));
+          }
+        });
+
+        console.log(checkedInputs);
+
+
+        $.ajax({
+          url: "/adminQuanLyPhim/ThemPhim",
+          type: "post",
+          processData: false,
+          mimeType: "multipart/form-data",
+          contentType: false,
+          data: formData,
+             
+          
+          success: function(data){
+              // xử lý khi thành công
+              // let result = JSON.parse(data);
+              console.log(data);
+          },
+          error: function(jqXHR, textStatus, errorThrown){
+              // xử lý khi lỗi
+              console.log(textStatus, errorThrown);
+          }
+      });
+     
+
+    });
+
+   
+
+    
+    
+
+
+});
+
+
